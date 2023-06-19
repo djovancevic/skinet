e8c8628d3220abe1c8b44336d8f757278bffb900
dejo <jovancevic.de@gmail.com>
2023-06-19 11:38:32 +0300

    whsecret added

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
-        private const string WhSecret = "whsec_90a72718423b4f5b87c329dab4b7c6b2f78d7406f00ffefeb901986f1be941ae";
+        private const string WhSecret = "whsec_fbdeefebca07675f80a07543a2dfc6760308741e0b6fdb813af8eced71d0435d";
         private readonly IPaymentService _paymentService;
         private readonly ILogger<PaymentsController> _logger;
         public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
