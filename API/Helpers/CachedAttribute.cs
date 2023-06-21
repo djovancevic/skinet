0055477fe133a7c4d645ec17824a55b3d1029f76
dejo <jovancevic.de@gmail.com>
2023-06-21 07:58:13 +0300

    feature: ready for publishing

---
+++ API/Helpers/CachedAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers;
+
+public class CachedAttribute : Attribute, IAsyncActionFilter
+{
+    private readonly int _timeToLiveSeconds;
+
+    public CachedAttribute(int timeToLiveSeconds)
+    {
+        _timeToLiveSeconds = timeToLiveSeconds;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var cacheService = 
+            context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+        var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+        var cachedResponse = await cacheService.GetCachedResponseAsync(cacheKey);
+        if (!string.IsNullOrEmpty(cachedResponse))
+        {
+            var contentResult = new ContentResult
+            {
+                Content = cachedResponse,
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+            context.Result = contentResult;
+            return;
+        }
+
+        var executedContext = await next(); // move to the controller
+        if (executedContext.Result is OkObjectResult okObjectResult)
+        {
+            await cacheService.CacheResponseAsync(cacheKey, okObjectResult.Value,
+                TimeSpan.FromSeconds(_timeToLiveSeconds));
+        }
+    }
+
+    private string GenerateCacheKeyFromRequest(HttpRequest httpContextRequest)
+    {
+        var keyBuilder = new StringBuilder();
+        keyBuilder.Append($"{httpContextRequest.Path}");
+        foreach(var (key, value) in httpContextRequest.Query.OrderBy(x => x.Key))
+        {
+            keyBuilder.Append($"|{key}-{value}");
+        }
+
+        return keyBuilder.ToString();
+    }
+}
